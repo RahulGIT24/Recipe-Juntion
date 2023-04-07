180b7199467a9d6ed28f4709d22e9628aab165e2
Rahul <rg4005450@gmail.com>
2023-04-07 20:39:02 +0530

    Completed

--- src/Components/RecipeItem.js
+++ src/Components/RecipeItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function RecipeItem(props) {
     const { title, image, calories, url, ingredients } = props;
+
     return (
         <>
             <div className="bg-black p-6 rounded-lg border-2 border-yellow">
                 <img className="h-40 rounded w-full object-cover object-center mb-6" src={image} alt="food" />
                 <h2 className="text-xl text-yellow font-medium title-font mb-4">{title}</h2>
                 <h2 className="text-xl text-white font-medium title-font mb-4">Calories - {calories}</h2>
-                <h3 className="tracking-widest text-yellow text-lg font-medium title-font">Ingredients</h3>
                 <a href={url} target='_blank' rel="noreferrer" className="text-yellow inline-flex items-center cursor-pointer hover:text-white">Steps to Cook
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
